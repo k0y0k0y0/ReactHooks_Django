adb79267caca209440def8f7807b9c0db4934fab
taak <papipupepo_10@yahoo.co.jp>
2020-12-30 16:01:14 +0900

    Done

---
+++ react-basic-drf/src/components/DrfApiFetch.js
@@ -0,0 +1,19 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+const DrfApiFetch = () => {
+
+const [tasks, setTasks] = useState([])
+
+useEffect(() => {
+  axios.get()
+})
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DrfApiFetch
